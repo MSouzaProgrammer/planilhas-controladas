8558cc748e43c8768671e6474dfd20c908b35210
MSouzaProgrammer <mateussouzaprogrammer@gmail.com>
2025-11-14 15:00:57 -0400

    Projeto incial

---
+++ src/main/java/com/seuprojeto/planilhas_app/PlanilhasAppApplication.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.planilhas_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanilhasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanilhasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seuprojeto/planilhas_app/PlanilhasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.planilhas_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanilhasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
